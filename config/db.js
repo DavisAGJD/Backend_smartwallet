7c1af0e208b38a7d13f32fa36a5857430b153773
David <avilagarciajesusdavid41@gmail.com>
2025-03-22 16:16:55 -0600

    cambio en db y cron

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql2");
-require('dotenv').config();  // Cargar las variables del archivo .env
+require("dotenv").config();  // Cargar las variables del archivo .env
 
 console.log("Intentando conectar con:", {
   host: process.env.DB_HOST,
@@ -9,20 +9,25 @@ console.log("Intentando conectar con:", {
   port: process.env.DB_PORT,
 });
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10, // Puedes ajustar este número según tus necesidades
+  queueLimit: 0,
 });
 
-db.connect((err) => {
+// Probar la conexión obteniendo una conexión del pool
+db.getConnection((err, connection) => {
   if (err) {
-    console.error(`Error al conectar la base de datos ${err}`);
+    console.error("Error al conectar la base de datos:", err);
     return;
   }
-  console.log("Conectando a la base de datos");
+  console.log("Conectado a la base de datos");
+  connection.release();
 });
 
-module.exports = db
+module.exports = db;
